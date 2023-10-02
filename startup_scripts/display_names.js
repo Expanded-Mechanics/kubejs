2d8b6cfd5c548fb4ae451eac4810b8e352ccb9be
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-02 11:46:58 -0300

    Added Expanded Mechanics Name to kube.js

---
+++ startup_scripts/display_names.js
@@ -0,0 +1,6 @@
+// Example of how to change the display name of a mod
+
+// Change the display name of the KubeJS mod
+// Platform.mods.{modname}.name = "mod name"
+
+Platform.mods.kubejs.name = "Expanded Mechanics"
