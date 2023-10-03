32067abd6081878731a3f74debdb8b5fab5c5890
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-03 18:54:15 -0300

    added slide_show modification

--- startup_scripts/blocks.js
+++ startup_scripts/blocks.js
@@ -1,9 +1,6 @@
-// Startup script, 1.18.2
-
-// add attributes to slide show block
-StartupEvents.registry("block" , (event) => {        
-    event.modify("slide_show:projector", (block) => {
-        block.destroySpeed = 0.2;
-        block.hasCollision = true;
-    });
+BlockEvents.modification((e) => {
+  e.modify("slide_show:projector", (block) => {
+    block.destroySpeed = 0.2;
+    block.hasCollision = true;
+  });
 });
