ed9894ee7ebc262782f5822e669e64ac4d100193
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-03 14:06:37 -0300

    Delete blocks.js

--- startup_scripts/blocks.js
+++
@@ -1,9 +0,0 @@
-// Startup script, 1.18.2
-
-// add attributes to slide show block
-StartupEvents.registry("block" , (event) => {        
-    event.modify("slide_show:projector", (block) => {
-        block.destroySpeed = 0.2;
-        block.hasCollision = true;
-    });
-});
